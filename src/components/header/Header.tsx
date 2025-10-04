0bbe0fb4f9343d0405043790ba43ea86925cfc1c
mast1f0 <avorobev602@gmail.com>
2025-10-04 13:52:03 +0300

    login test

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,6 @@
 import SettingsButton from "../elements/buttons/SettingsButton";
 import ThreeDots from "../elements/buttons/Three-dots";
 import UserPanel from "../elements/UserPanel/UserPanel";
-// import PhoneButton from "../elements/buttons/PhoneButton";
 import SearchInput from "../elements/SearchInput";
 import "./Header.style.css";
 import AddFriendButton from "../elements/buttons/AddFriend";
