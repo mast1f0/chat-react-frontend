b7b8e1a3438faccf6aa1389606c931d538c31f4d
mast1f0 <avorobev602@gmail.com>
2025-10-04 20:42:34 +0300

    start message input

---
+++ src/components/elements/InputMessage/InputMessage.tsx
@@ -0,0 +1,19 @@
+import "./InputMessage.style.css";
+
+export default function InputMessage() {
+  return (
+    <div className="input-message__wrapper">
+      <div className="input-message">
+        <input type="text" id="message" placeholder="Напишите сообщение" />
+        <div className="input__icons">
+          <img src="src/assets/mic.svg" alt="Голосовое" />
+          <img src="src/assets/scrap.svg" alt="Прикрепить файл" />
+        </div>
+      </div>
+
+      <button className="send-btn">
+        <img src="src/assets/airplane.svg" alt="" />
+      </button>
+    </div>
+  );
+}
--- src/components/elements/chatSection/ChatSection.tsx
+++ src/components/elements/chatSection/ChatSection.tsx
@@ -1,9 +1,14 @@
 import "./ChatSection.style.css";
+import InputMessage from "../InputMessage/InputMessage";
 
 export default function ChatSection() {
   return (
-    <div style={{ display: "flex" }} className="main-section">
-      e
+    <div
+      style={{ display: "flex", flexDirection: "column", height: "100vh" }}
+      className="main-section"
+    >
+      <div style={{ maxHeight: "80%", flex: 1 }}></div>
+      <InputMessage />
     </div>
   );
 }
