8c60a5e5c2acdfb9d3c5c625b2a2b9db3ea132ce
mast1f0 <avorobev602@gmail.com>
2025-11-05 19:33:14 +0300

    revert settings

--- src/components/buttons/SettingsButton.tsx
+++ src/components/buttons/SettingsButton.tsx
@@ -1,14 +1,16 @@
+import { ReactSVG } from "react-svg";
 import settingsIcon from "../../assets/setting-button.svg";
 export default function SettingsButton() {
   return (
     <a
       href="/settings"
       aria-label="Настройки"
-      className="md:w-8 md:h-8 h-14 flex-1 flex-shrink min-w-0 basis-0 rounded-[15px] bg-[#E1E0E1] md:bg-transparent rounded-b-lg flex items-center justify-center no-underline transform transition-transform duration-200 hover:scale-110 md:hover:scale-125 overflow-hidden"
+      className="md:w-8 md:h-8 h-14 max-w-12 rounded-[15px] px-[45px] bg-[#E1E0E1] md:bg-transparent rounded-b-lg flex items-center justify-center no-underline transform transition-transform duration-200 hover:scale-125"
     >
-      <img
-        className="btn-icon flex-shrink-0 w-8 h-8 sm:w-10 sm:h-10"
+      <ReactSVG
+        className="btn-icon"
         src={settingsIcon}
+        style={{ maxWidth: 51, maxHeight: 51 }}
       />
     </a>
   );
